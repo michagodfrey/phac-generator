bb1f518103edd6051d7ca8f407ed4b12e32499f5
Michael Godfrey <m.godfrey59@gmail.com>
2025-05-24 21:23:06 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# PHAC Generator
---
+++ app.js
@@ -0,0 +1,47 @@
+// phac_app_script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const produceContainer = document.getElementById("produceEntries");
+  const addProduceBtn = document.getElementById("addProduceEntry");
+  let produceCount = 1;
+
+  addProduceBtn.addEventListener("click", () => {
+    if (produceCount >= 4) return;
+
+    const newSet = document.createElement("div");
+    newSet.classList.add("produce-set");
+    newSet.innerHTML = `
+      <hr style='margin: 1rem 0;'>
+      <input type="text" name="numberOfPackages[]" placeholder="Number of Packages" class="input" />
+      <input type="text" name="packageType[]" placeholder="Type of Packages (e.g., trays, cartons)" class="input" />
+      <input type="text" name="carrierType[]" placeholder="Type of Carrier of Biosecurity Matter" class="input" />
+    `;
+    produceContainer.appendChild(newSet);
+    produceCount++;
+  });
+
+  const accreditationBtn = document.getElementById("addAccreditation");
+  const certFieldset = accreditationBtn.closest("fieldset");
+
+  accreditationBtn.addEventListener("click", () => {
+    const newBlock = document.createElement("div");
+    newBlock.classList.add("accreditation-block");
+    newBlock.innerHTML = `
+      <hr style='margin: 1rem 0;'>
+      <input type="text" name="procedureCode[]" placeholder="Procedure Code (e.g., ICA-39)" class="input" />
+      <input type="number" name="facilityNumber[]" placeholder="Facility Number" class="input" />
+      <input type="date" name="accreditationExpiry[]" placeholder="Accreditation Expiry Date" class="input" />
+    `;
+    certFieldset.appendChild(newBlock);
+  });
+
+  // Toggle grower/certifier fields
+  const sameAsCheckbox = document.querySelector(
+    "input[name='sameAsCertifier']"
+  );
+  const certifierFields = document.getElementById("certifierFields");
+
+  sameAsCheckbox.addEventListener("change", (e) => {
+    certifierFields.style.display = e.target.checked ? "none" : "block";
+  });
+});
